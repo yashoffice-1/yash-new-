5799d5a5fd8a00a50048f53e75d76b0df8566e9d
Het <het.sojitra4399@gmail.com>
2025-07-21 12:20:07 +0530

    Removed the migration files

--- supabase/migrations/20250703110017-48ea29a0-54ea-45d1-a3fb-043146cc79f6.sql
+++
@@ -1,3 +0,0 @@
--- Add gif_url column to asset_library table for storing HeyGen GIF previews
-ALTER TABLE public.asset_library 
-ADD COLUMN gif_url TEXT;
--- supabase/migrations/20250703183632-19fe0fef-d916-40ce-b083-f1625bb4e6c8.sql
+++
@@ -1,5 +0,0 @@
--- Update existing external video URLs to show failed status
-UPDATE asset_library 
-SET asset_url = 'failed' 
-WHERE asset_url LIKE 'https://dnznrvs05pmza.cloudfront.net%' 
-  AND asset_type = 'video';
